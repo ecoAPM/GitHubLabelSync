30557b905f4ef659028aeac5e4f4b1d91ce4d3f1
Steve Desmond <Steve@ecoAPM.com>
2023-10-16 14:46:51 -0400

    Upgrade to Octokit v8.1

--- test/Stubs/Repository.cs
+++ test/Stubs/Repository.cs
@@ -5,7 +5,7 @@ namespace GitHubLabelSync.Tests.Stubs;
 public class Repository : Octokit.Repository
 {
 	public Repository(string name, bool archived = false)
-		: base(null, null, null, null, null, null, null, 0, null, null, name, null, false, null, null, null, false, false, 0, 0, null, 0, null, DateTimeOffset.Now, DateTimeOffset.Now, null, null, null, null, false, false, false, false, false, 0, 0, null, null, null, archived, 0, null, RepositoryVisibility.Public, Array.Empty<string>(), null, null, null)
+		: base(null, null, null, null, null, null, null, null, 0, null, null, name, null, false, null, null, null, false, false, 0, 0, null, 0, null, DateTimeOffset.Now, DateTimeOffset.Now, null, null, null, null, false, false, false, false, false, 0, 0, null, null, null, archived, 0, null, RepositoryVisibility.Public, Array.Empty<string>(), null, null, null)
 	{
 	}
 }
