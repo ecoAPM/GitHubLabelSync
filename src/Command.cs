fc353d541493659dc95ae501d63a50b128b6a254
Steve Desmond <Steve@ecoAPM.com>
2025-11-04 16:54:34 -0500

    Upgrade Spectre CLI to v0.53

--- src/Command.cs
+++ src/Command.cs
@@ -10,7 +10,10 @@ public class Command : AsyncCommand<Settings>
 	public Command(IAnsiConsole console)
 		=> _console = console;
 
-	public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+	public async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+		=> await ExecuteAsync(context, settings, CancellationToken.None);
+
+	public override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
 	{
 		try
 		{
